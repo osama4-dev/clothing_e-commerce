111f1b7c4a3b884aad6779bf3ae0853ff35450c0
Your Name <you@example.com>
2021-04-05 20:08:52 +0500

    we made COLLECTION_ID_MAP in our shop selector to match our string values to our integer id we made a new selector called selectCollection where we mapped ourstring url we then called it in our collection.component.jsx file we use passed it through mapStateToProps

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 
 import CollectionItem from '../../components/collection-item/collection-item.component'
-
+import {connect} from 'react-redux'
+import {selectCollection} from '../../redux/shop/shop.selector'
 import './collection.styles.scss'
 
 
-const CollectionPage = ({match}) => {
-    console.log(match.params.collectionId)
-    // so we are getting hats in our console.log after passing this console.log(match.params.categoryId)
+const CollectionPage = ({collection}) => {
+    console.log(collection)
+    // so we are getting hats in our console.log after passing this console.log(match.params.categoryId) which we 
+    //were passing before but now istead of that we pass collection as we moidified our code 
     return(
     <div className="collection-page">
     <h2>Collection Page</h2></div>
 )}
+//we pass here the first parameter as 'state' which is the overall reducer state from the top
+//the 2nd argument is the 'ownProps' which is the props of the component we are wrapping in the connect
 
-export default CollectionPage
+//we are also passing the state here unline the other selectors were passed in prvs files 
+//that is because it is necessary because unlike other selectors,this selector needs a part of the state
+//depending on the URL parameter this is coming from the shop.selector
+const mapStateToProps =(state,ownProps) =>({
+    collection:selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
+
+//ya hm araha hai shop.selector.js kee file sa continuing where we use ownProps which gives us all our props that
+//is on our collection page component including our mathc object that we are getting from our Route
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,38 @@
 import {createSelector} from 'reselect'
 
+//collection_id_map which is used for mapping the string value to the respective id because when 
+//we were passing   console.log(match) we were getting the id values which were number and then the 
+//items with like the title,name and more so these were in strings while the id was in number so we either have to
+//convert the string to integers or the integer to string,so now the string value we are getting from the url
+//parameter in our case its shop/hats will be the actual property and give hats the id of 1 and same
+//for every other that is sneakers,jackets,men for this we are also use selectCollection where we all finding
+//you can find more about it in the assets an image is saved with  the name "For the unique id"
+
+//aba bhai dekh jo id thee wo number m thee to hmna yaha us id ko string sa represtn krdia hai
+//no.1 id p hats the islia hmna uska brabar show krdia ussa hats:1 krka yanee ab id:1 ko hm hats sa bhee
+//represent krskta hai pher hm map krta hai by selector called selectCollection pher url parameter ata hai
+//jo k string hai "collectionUrlParam" and we return the createSelector which is called a curried function 
+//pher hm apna collections array uthata hai jo k yaha "collections" hai or pher hm uspa find lgata hai jaha
+//pher hm match kr rha hai correct id to our  "COLLECTION_ID_MAP" using our string paramter value that we get
+//from the url
+//or pher jao collection.component.jsx kee file m
+const COLLECTION_ID_MAP = {
+hats:1,
+sneakers:2,
+jackets:3,
+umm:4,
+mens:5
+}
+
 const selectShop  = state => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop],
     shop=>shop.collections
+)
+
+export const selectCollection = collectionUrlParam => 
+createSelector(
+    [selectCollections],
+    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
 )
